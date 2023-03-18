2536634db8579abe239b6b0b27f9d9dc3515fef0
airRnot1106 <yngw.rintaro@gmail.com>
2023-03-18 15:03:40 +0900

    env: vitestの導入
    command: yarn add -D vitest jsdom @vitejs/plugin-react @testing-library/react @testing-library/jest-dom @testing-library/user-event @types/testing-library__user-event eslint-plugin-vitest

--- .eslintrc.cjs
+++ .eslintrc.cjs
@@ -7,9 +7,30 @@ module.exports = {
     'plugin:storybook/recommended',
     'prettier',
   ],
-  plugins: ['@typescript-eslint'],
+  plugins: ['@typescript-eslint', 'vitest'],
   parser: '@typescript-eslint/parser',
   parserOptions: {
     sourceType: 'module',
+  },
+  rules: {
+    /* vitest */
+    'vitest/consistent-test-it': [
+      'error',
+      {
+        fn: 'test',
+      },
+    ],
+    'vitest/expect-expect': 'warn',
+    'vitest/lower-case-title': 'off',
+    'vitest/max-nested-describe': [
+      'error',
+      {
+        max: 2,
+      },
+    ],
+    'vitest/no-conditional-tests': 'error',
+    'vitest/no-focused-tests': 'warn',
+    'vitest/no-identical-title': 'error',
+    'vitest/no-skipped-tests': 'warn',
   },
 };
