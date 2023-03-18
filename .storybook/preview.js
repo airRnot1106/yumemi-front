b4bb5d52ceb908b5784101c4356d626032149493
airRnot1106 <yngw.rintaro@gmail.com>
2023-03-18 14:58:26 +0900

    env: storybookの追加設定
    command: yarn add -D @storybook/addon-a11y css-loader sass-loader storybook-addon-next-router style-loader

--- .storybook/preview.js
+++ .storybook/preview.js
@@ -1,9 +1,25 @@
+import { RouterContext } from 'next/dist/shared/lib/router-context';
+
+import * as NextImage from 'next/image';
+
+const OriginalNextImage = NextImage.default;
+
+Object.defineProperty(NextImage, 'default', {
+  configurable: true,
+  value: (props) => <OriginalNextImage {...props} unoptimized />,
+});
+
+import '@/app/globals.scss';
+
 export const parameters = {
-  actions: { argTypesRegex: "^on[A-Z].*" },
+  actions: { argTypesRegex: '^on[A-Z].*' },
   controls: {
     matchers: {
       color: /(background|color)$/i,
       date: /Date$/,
     },
   },
-}
+  nextRouter: {
+    Provider: RouterContext.Provider,
+  },
+};
