c9b28e1f93330c316734090cf3c44a5fda50579c
airRnot1106 <yngw.rintaro@gmail.com>
2023-03-18 17:06:40 +0900

    feat: SiteHeaderを追加

---
+++ src/components/feature/site/molecules/SiteHeader.tsx
@@ -0,0 +1,19 @@
+import styles from '@/components/feature/site/molecules/SiteHeader.module.scss';
+
+export type SiteHeaderPresenterProps = {
+  title: string;
+};
+
+export const SiteHeaderPresenter = ({ title }: SiteHeaderPresenterProps) => {
+  return (
+    <header className={styles['module']}>
+      <div>
+        <h1 className={styles['title']}>{title}</h1>
+      </div>
+    </header>
+  );
+};
+
+export const SiteHeader = () => {
+  return <SiteHeaderPresenter title="Yumemi-Front" />;
+};
