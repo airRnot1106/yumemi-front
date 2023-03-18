b2fbec6130d44252c616e90d9561f01d704f5c1f
airRnot1106 <yngw.rintaro@gmail.com>
2023-03-18 14:50:48 +0900

    env: eslintとprettierの基本設定
    command: yarn add -D @typescript-eslint/eslint-plugin @typescript-eslint/parser eslint-config-prettier prettier

---
+++ .eslintrc.cjs
@@ -0,0 +1,14 @@
+module.exports = {
+  root: true,
+  extends: [
+    "next/core-web-vitals",
+    "eslint:recommended",
+    "plugin:@typescript-eslint/recommended",
+    "prettier",
+  ],
+  plugins: ["@typescript-eslint"],
+  parser: "@typescript-eslint/parser",
+  parserOptions: {
+    sourceType: "module",
+  },
+};
